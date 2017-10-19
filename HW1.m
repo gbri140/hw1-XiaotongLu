ed935feaeb527045d986374caf9faa57c7a214fe
gbri140 <glb3@rice.edu>
2017-10-19 13:46:41 -0500

    Update HW1.m

--- HW1.m
+++ HW1.m
@@ -1,3 +1,17 @@
+GB comments:
+Prob1: 100%
+Prob2:
+P1:100 
+P2: 50 the code does not consider whether the ORF is in correct reading frame. 
+P3:100
+P4:100
+P5:100 
+Prob3
+P1: 100 
+P2:100
+P3:0 
+Overall: 83
+
 % Homework 1. Due before class on 9/5/17
 
 %% Problem 1 - addition with strings
